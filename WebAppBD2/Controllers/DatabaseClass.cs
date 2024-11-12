5d940b4448c7634e996cdc8d2d6d5c65b9691d1d
Budancev <budantsev03@mail.ru>
2024-11-12 17:51:16 +0700

    Веб приложение завершено. Дописан первый запрос. Сформированы логи. Стилизирован сайт.

--- WebAppBD2/Controllers/DatabaseClass.cs
+++ WebAppBD2/Controllers/DatabaseClass.cs
@@ -1,21 +1,29 @@
 ﻿using Npgsql;
+using WebAppBD2.Controllers;
 
-class Database_Queries
+class Database_Queries : IDisposable
 {
 	public NpgsqlConnection? Connection_BD { get; set; }
+    private readonly ILogger<HomeController> _logger;
 
-	public void ConnectToBD()
+	public Database_Queries(ILogger<HomeController> logger) {
+		_logger = logger;
+	}
+
+    public void ConnectToBD()
 	{
 		string connString = "Host=students.ami.nstu.ru;Username=pmi-b1309;Password=MalCeer0;Database=students;Search Path=pmib1309";
 		try
 		{
 			this.Connection_BD = new NpgsqlConnection(connString);
 			this.Connection_BD.Open();
-			Console.WriteLine("Соединение открыто\n");
+			_logger.LogInformation("Connection open...");
 		}
 		catch (Exception ex)
 		{
 			this.CloseConnectToBD();
+			_logger.LogCritical("The connection is closing...");
+			_logger.LogCritical(ex.Message);
 			throw new Exception(ex.Message);
 		}
 	}
@@ -23,102 +31,204 @@ class Database_Queries
 
 	public List<string>? GetListTownPost()
 	{
-		if (this.Connection_BD == null) return null;
+		if (this.Connection_BD == null)
+		{
+			_logger.LogError("No connection to the server.");
+			return null;
+		}
 		List<string> towns = [];
 		try
 		{
 			var transaction = this.Connection_BD.BeginTransaction();
+			_logger.LogInformation("Starting a transaction...");
 			var command = this.Connection_BD.CreateCommand();
 			command.Transaction = transaction;
 			command.CommandText = "SELECT distinct town FROM p";
-			try
+            _logger.LogInformation("Create command: {}", command.CommandText);
+            try
 			{
 				var reader = command.ExecuteReader();
-				if (reader.HasRows)
+				_logger.LogInformation("Sending command to server. Receiving result...");
+				if (reader.HasRows) {
+					_logger.LogInformation("Information received. Processing data...");
 					while (reader.Read())
 						towns.Add(reader.GetString(0));
-				else
+                }
+                else
 				{
+					_logger.LogInformation("Empty result received. Transaction completed.");
 					reader.Close();
 					return null;
 				}
+				_logger.LogInformation("Data processed. Completing transaction...");
 				reader.Close();
 				transaction.Commit();
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex.Message);
+				_logger.LogWarning(ex.Message);
+				_logger.LogInformation("Rollback transaction...");
 				transaction.Rollback();
 			}
+			_logger.LogInformation("Transaction completed. Data returned.");
 			return towns;
 		}
 		catch (Exception ex)
 		{
+            _logger.LogCritical("The connection is closing...");
+            _logger.LogCritical(ex.Message);
 			this.CloseConnectToBD();
 			throw new Exception(ex.Message);
 		}
 	}
 	public object? ExecuteScalarCommand(string com)
 	{
-		if (this.Connection_BD == null) return null;
+		if (this.Connection_BD == null)
+		{
+			_logger.LogError("No connection to the server.");
+			return null;
+		}
 		object? result = null;
 		try
 		{
 			var transaction = this.Connection_BD.BeginTransaction();
-			var command = this.Connection_BD.CreateCommand();
+            _logger.LogInformation("Starting a transaction...");
+            var command = this.Connection_BD.CreateCommand();
 			command.Transaction = transaction;
 			command.CommandText = com;
-			try
+            _logger.LogInformation("Create command: {}", command.CommandText);
+            try
 			{
 				result = command.ExecuteScalar();
-				transaction.Commit();
+                _logger.LogInformation("Sending command to server. Information received. Completing transaction...");
+                transaction.Commit();
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex.Message);
-				transaction.Rollback();
+                _logger.LogWarning(ex.Message);
+                _logger.LogInformation("Rollback transaction...");
+                transaction.Rollback();
 			}
-			return result;
+            _logger.LogInformation("Transaction completed. Data returned.");
+            return result;
 		}
 		catch (Exception ex)
 		{
-			this.CloseConnectToBD();
+            _logger.LogCritical("The connection is closing...");
+            _logger.LogCritical(ex.Message);
+            this.CloseConnectToBD();
 			throw new Exception(ex.Message);
 		}
 
 	}
 	public int ExecuteNonQueryCommand(string com) {
-		if (this.Connection_BD == null) return 0;
+		if (this.Connection_BD == null) {
+            _logger.LogError("No connection to the server.");
+            return 0;
+		}
 		int num = 0;
         try
         {
             var transaction = this.Connection_BD.BeginTransaction();
+            _logger.LogInformation("Starting a transaction...");
             var command = this.Connection_BD.CreateCommand();
             command.Transaction = transaction;
             command.CommandText = com;
+            _logger.LogInformation("Create command: {}", command.CommandText);
             try
             {
                 num = command.ExecuteNonQuery();
+                _logger.LogInformation("Sending command to server. Completing transaction...");
                 transaction.Commit();
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                _logger.LogWarning(ex.Message);
+                _logger.LogInformation("Rollback transaction...");
                 transaction.Rollback();
             }
+            _logger.LogInformation("Transaction completed. Data returned.");
             return num;
         }
         catch (Exception ex)
         {
+            _logger.LogCritical("The connection is closing...");
+            _logger.LogCritical(ex.Message);
             this.CloseConnectToBD();
             throw new Exception(ex.Message);
         }
 
     }
+	public Dictionary<string, List<string?>>? ExecuteReaderCommand(string com) {
+		if (this.Connection_BD == null)
+		{
+            _logger.LogError("No connection to the server.");
+            return null;
+		}
+		Dictionary<string, List<string?>> result = [];
+		try
+        {
+            var transaction = this.Connection_BD.BeginTransaction();
+            _logger.LogInformation("Starting a transaction...");
+            var command = this.Connection_BD.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandText = com;
+            _logger.LogInformation("Create command: {}", command.CommandText);
+            try
+            {
+				var reader = command.ExecuteReader();
+                _logger.LogInformation("Sending command to server. Receiving result...");
+                if (reader.HasRows)
+                {
+                    _logger.LogInformation("Information received. Processing data...");
+                    for (int i = 0; i < reader.FieldCount; i++)
+						result.Add(reader.GetName(i), []);
+					while (reader.Read())
+						for (int i = 0; i < reader.FieldCount; i++)
+						{
+							string key = reader.GetName(i);
+							string? value = reader.GetValue(i).ToString();
+							result[key].Add(value);
+						}
+				}
+                else
+                {
+                    _logger.LogInformation("Empty result received. Transaction completed.");
+                    reader.Close();
+                    transaction.Commit();
+                    return null;
+                }
+                _logger.LogInformation("Data processed. Completing transaction...");
+                reader.Close();
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex.Message);
+                _logger.LogInformation("Rollback transaction...");
+                transaction.Rollback();
+            }
+            _logger.LogInformation("Transaction completed. Data returned.");
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical("The connection is closing...");
+            _logger.LogCritical(ex.Message);
+            this.CloseConnectToBD();
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public void Dispose()
+    {
+        _logger.LogCritical("The connection is closing...");
+        this.CloseConnectToBD();
+    }
 
     ~Database_Queries()
 	{
-		this.CloseConnectToBD();
+		Dispose();
 	}
 
 }
--- WebAppBD2/Controllers/HomeController.cs
+++ WebAppBD2/Controllers/HomeController.cs
@@ -2,17 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebAppBD2.Models;
 using Microsoft.AspNetCore.Html;
+using System.Text;
 
 namespace WebAppBD2.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller, IDisposable
     {
         private readonly ILogger<HomeController> _logger;
         private readonly Database_Queries ServerBD;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            ServerBD = new Database_Queries();
+            ServerBD = new Database_Queries(_logger);
+            _logger.LogInformation("Connecting to Server: students.ami.nstu.ru");
             ServerBD.ConnectToBD();
         }
 
@@ -27,68 +29,139 @@ namespace WebAppBD2.Controllers
             }
             return line;
         }
+
+        public static string ConvertToHtmlTable(Dictionary<string, List<string?>> data)
+        {
+            StringBuilder html = new();
+
+            html.AppendLine("<table class='table'>");
+            html.AppendLine("<thead>");
+            html.AppendLine("<tr>");
+
+            // Заголовки таблицы
+            foreach (var header in data.Keys)
+                html.AppendLine($"<th>{header}</th>");
+            
+            html.AppendLine("</tr>");
+            html.AppendLine("</thead>");
+            html.AppendLine("<tbody>");
+
+            // Определяем максимальное количество строк
+            int maxRows = 0;
+            foreach (var list in data.Values)
+                if (list.Count > maxRows)
+                    maxRows = list.Count;
+
+            // Заполнение строк таблицы
+            for (int i = 0; i < maxRows; i++)
+            {
+                html.AppendLine("<tr>");
+                foreach (var key in data.Keys)
+                {
+                    var value = i < data[key].Count ? data[key][i] : "";
+                    html.AppendLine($"<td>{(value ?? "NULL")}</td>");
+                }
+                html.AppendLine("</tr>");
+            }
+
+            html.AppendLine("</tbody>");
+            html.AppendLine("</table>");
+
+            return html.ToString();
+        }
+
         public IActionResult ResultTask(string data) {
-			ViewBag.results = data;
+			ViewBag.results = new HtmlString(data);
 			return View();
         }
         public IActionResult Index()
         {
-
+            _logger.LogInformation("Start processing GET-request for page: {}", "Task 1");
+            _logger.LogInformation("Formation of Option List");
             string? result = GetOptionString();
-            if (result is null)
+            if (result == null)
             {
-                ViewBag.TownPost = new HtmlString("<input type=\"text\" name=\"town_task_1\" />");
+                _logger.LogWarning("Option List not Formed. Forming the input field...");
+                ViewBag.TownPost = new HtmlString("<input class=\"text-field__input\" type=\"text\" name=\"town_task_1\" />");
             }
             else
             {
+                _logger.LogInformation("Option List Formed.");
                 ViewBag.TownPost = new HtmlString($"<select name=\"town_task_1\">{result}</select>");
             }
+            _logger.LogInformation("Forming date input fields");
+            _logger.LogInformation("Getting the smallest date from the database.");
             object? min_data = this.ServerBD.ExecuteScalarCommand("select MIN(date_post) from spj1");
+            _logger.LogInformation("Getting the largest date from the database.");
             object? max_data = this.ServerBD.ExecuteScalarCommand("select MAX(date_post) from spj1");
-            result = "<input type=\"date\" name=\"time_start\" ";
-            if (min_data is not null)
-            {
-                result += $"value=\"{((DateTime)min_data).ToString("yyyy-MM-dd")}\" min=\"{((DateTime)min_data).ToString("yyyy-MM-dd")}\" ";
-            }
-            if (max_data is not null)
-            {
-                result += $"max=\"{((DateTime)max_data).ToString("yyyy-MM-dd")}\" ";
-            }
+            _logger.LogInformation("Start forming the first date input field");
+            result = "<input class=\"text-field__input\" type=\"date\" name=\"time_start\" ";
+            if (min_data != null)
+                result += $"value=\"{(DateTime)min_data:yyyy-MM-dd}\" min=\"{(DateTime)min_data:yyyy-MM-dd}\" ";
+            else
+                _logger.LogWarning("Lower limit missed...");
+            if (max_data != null)
+                result += $"max=\"{(DateTime)max_data:yyyy-MM-dd}\" ";
+            else
+                _logger.LogWarning("Upper limit is missing");
             result += "/>";
             ViewBag.time_start = new HtmlString(result);
-            result = "<input type=\"date\" name=\"time_end\" ";
-            if (min_data is not null)
-            {
-                result += $"min=\"{((DateTime)min_data).ToString("yyyy-MM-dd")}\" ";
-            }
-            if (max_data is not null)
-            {
-                result += $"value=\"{((DateTime)max_data).ToString("yyyy-MM-dd")}\" max=\"{((DateTime)max_data).ToString("yyyy-MM-dd")}\" ";
-            }
+            _logger.LogInformation("The first date input field has been generated successfully.");
+            _logger.LogInformation("Start forming the second date input field");
+            result = "<input class=\"text-field__input\" type=\"date\" name=\"time_end\" ";
+            if (min_data != null)
+                result += $"min=\"{(DateTime)min_data:yyyy-MM-dd}\" ";
+            else
+                _logger.LogWarning("Lower limit missed...");
+            if (max_data != null)
+                result += $"value=\"{(DateTime)max_data:yyyy-MM-dd}\" max=\"{(DateTime)max_data:yyyy-MM-dd}\" ";
+            else
+                _logger.LogWarning("Upper limit is missing");
             result += "/>";
             ViewBag.time_end = new HtmlString(result);
+            _logger.LogInformation("The second date input field has been generated successfully.");
+            _logger.LogInformation("Get request processing for the page {} completed", "Task 1");
             return View();
         }
         [HttpPost]
         public RedirectToActionResult Index(string town_task_1, DateTime time_start, DateTime time_end) {
-            string result = $"Town: {town_task_1}; Post On: {time_start}; Post To: {time_end}";
-            return RedirectToAction("ResultTask", "Home", new { data = result });
+            _logger.LogInformation("Start processing POST-request for page: {}", "Task 1");
+            _logger.LogInformation("Getting the results of task 1...");
+            Dictionary<string, List<string?>>? result = this.ServerBD.ExecuteReaderCommand($"SELECT DISTINCT s.* FROM spj1 JOIN s ON spj1.n_post = s.n_post WHERE date_post >= to_date('{time_start:dd-MM-yyyy}','dd-mm-yyyy') and date_post <= to_date('{time_end:dd-MM-yyyy}','dd-mm-yyyy') and spj1.n_det IN(SELECT DISTINCT p.n_det FROM p WHERE p.town = '{town_task_1.Trim()}')");
+            if (result == null)
+            {
+                _logger.LogWarning("No results found. Generating page with results.");
+                _logger.LogInformation("POST-request processing for the page {} completed", "Task 1");
+                return RedirectToAction("ResultTask", "Home", new { data = "<p>Результаты не найденыю.</p>" });
+            }
+            _logger.LogInformation("Generating page with results.");
+            _logger.LogInformation("POST-request processing for the page {} completed", "Task 1");
+            return RedirectToAction("ResultTask", "Home", new { data = ConvertToHtmlTable(result) });
         }
 
         [HttpPost]
         public RedirectToActionResult Privacy(string name_post, int reiting, string town) {
+            _logger.LogInformation("Start processing POST-request for page: {}", "Task 2");
+            _logger.LogInformation("Getting the maximum supplier number...");
             object? data = ServerBD.ExecuteScalarCommand("SELECT MAX(n_post) FROM s");
             int max_val = 0;
-            if (data is not null)
+            if (data != null)
                 max_val = int.Parse(data.ToString()[1..]);
+            else
+                _logger.LogInformation("The table with suppliers is empty");
             string n_post = "S" + (max_val + 1);
+            _logger.LogInformation("Getting the results of task 2...");
             int num = this.ServerBD.ExecuteNonQueryCommand($"INSERT INTO s VALUES ('{n_post}','{name_post}',{reiting},'{town}');");
             if (num > 0)
             {
-                return RedirectToAction("ResultTask", "Home", new { data = $"Добавлена {num} запись в таблицу 's'." });
+                _logger.LogInformation("The entry was added successfully.");
+                _logger.LogInformation("POST-request processing for the page {} completed", "Task 2");
+                return RedirectToAction("ResultTask", "Home", new { data = $"<p>Добавлена {num} запись в таблицу 's'.</p>" });
             }
             else {
-                return RedirectToAction("ResultTask", "Home", new { data = $"Добавлено {num} записей в таблицу 's'." });
+                _logger.LogWarning("The entry has not been added.");
+                _logger.LogInformation("POST-request processing for the page {} completed", "Task 2");
+                return RedirectToAction("ResultTask", "Home", new { data = $"<p>Добавлено {num} записей в таблицу 's'.</p>" });
             }
         }
 
@@ -102,8 +175,11 @@ namespace WebAppBD2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        ~HomeController() {
+        public void Dispose() {
             ServerBD.CloseConnectToBD();
+        }
+        ~HomeController() {
+            Dispose();
         }
     }
 }
